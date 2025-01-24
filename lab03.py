2f2bb959a739fee0a7c8ffffd070f30af43a1162
khalidwasim09 <khalidwasimalam@gmail.com>
2025-01-24 09:46:23 -0500

    add full solution

--- lab03.py
+++ lab03.py
@@ -1,1 +1,9 @@
 print("Hello World!")
+#Dice options using list() and range()
+diceOptions = list(range(1,700))
+
+#Weapons Array 
+weapons = ["Fist", "Knife", "Club", "Gun", "Bomb", "Nuclear Bomb"] 
+
+#Display available options 
+print("Available Weapons: ")
